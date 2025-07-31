5a40e7704389641d17abfe8d4e3dc67fc1c17fd3
unknown <quocbao.job106204@gmail.com>
2025-08-01 01:33:33 +0700

    refactor: apply private middleware

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,19 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tnqbao/gau-upload-service/controller"
+	"github.com/tnqbao/gau-upload-service/middlewares"
 )
 
 func SetupRouter(ctrl *controller.Controller) *gin.Engine {
 	r := gin.Default()
+	middles, err := middlewares.NewMiddlewares(ctrl)
+	if err != nil {
+		panic(err)
+	}
+
 	apiRoutes := r.Group("/api/v2/upload")
 	{
+		apiRoutes.Use(middles.PrivateMiddlewares)
 		apiRoutes.PATCH("/image", ctrl.UploadImage)
 
 	}
